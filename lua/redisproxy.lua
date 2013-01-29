227ffa92c9f65fd19ebc53edddb1eb7d9ae32cfc
cloudaice <cloudaice@163.com>
2013-01-29 17:47:23 +0800

    add pattern

--- lua/redisproxy.lua
+++ lua/redisproxy.lua
@@ -95,6 +95,22 @@ elseif pattern == 2 then
     local cmd = uri_args[#uri_args]
     local args_string = struct_args(redis_args)
     red[cmd](red,loadstring(args_string)())
+elseif pattern == 3 then
+    local key = uri_args[3] 
+    local field = uri_args[5]
+    local cmd = uri_args[#uri_args]
+    local args_string = struct_args(redis_args)
+    red[cmd](red,key,field,loadstring(args_string)())
+elseif pattern == 4 then
+    local key = uri_args[3]
+    local member = uri_args[5]
+    local cmd = uri_args[#uri_args]
+    local args_string = struct_args(redis_args)
+    red[cmd](red,key,member,loadstring(args_string)())
+else
+    ngx.log(ngx.INFO, 'error pattern')
+    ngx.exit(500)
+end
 
 local ok, err = red:set_keepalive(10000,100)
 if not ok then
