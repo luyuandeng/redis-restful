8ea3e2e0ff4df4e82bffb3c3156164a46624bf23
cloudaice <cloudaice@163.com>
2013-05-02 10:25:59 +0200

    update test file

--- test/test.py
+++ test/test.py
@@ -1,16 +1,19 @@
 #-*-coding: utf-8 -*-
 # sudo pip install requests
-import  requests
-import json
-import random
-import md5
+import requests
+#import json
+#import random
+#import md5
 
 
 apps = {'appid', 'default'}
 types = {'key', 'keys'}
 
-if __name__ == "__main__":
-    form = {'value':'www.google.com'}
-    r = requests.post('http://localhost:8080/default/key/google/set', data = form)
-    print r.text
 
+def Test_set():
+    form = {'value': 'www.google.com'}
+    r = requests.post('http://localhost:8080/default/key/google/set', data=form)
+    assert r.response_code == 200
+
+if __name__ == "__main__":
+    Test_set()
